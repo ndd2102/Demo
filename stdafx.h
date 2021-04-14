775611c784be6a8f06fdbe2158f2964584dff5a3
ndd2102 <78080603+ndd2102@users.noreply.github.com>
2021-04-14 16:09:06 +0700

    Add files via upload

---
+++ stdafx.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include <SDL_image.h>
+#include <SDL.h>
+#include <string>
+#include <vector>
+#include <stdio.h>
+#include <SDL_mixer.h>
+//#include <SDL_ttf.h>
+#include <ctime>
+#include <stdlib.h>
+using namespace std;
+
+const int SCREEN_WIDTH = 600;
+const int SCREEN_HEIGHT = 800;
+const int speed = 5;
+
+
